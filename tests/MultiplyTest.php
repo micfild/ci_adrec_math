384f1bee089e639872e0794826780aca60208a59
JulienV-program <julien.valliorgue@free.fr>
2019-08-27 15:15:56 +0200

    fixed unit test name

--- tests/MultiplyTest.php
+++ tests/MultiplyTest.php
@@ -16,22 +16,22 @@ class MultiplyTest extends TestCase
         $this->multiply = new Multiply();
     }
 
-    public function testA()
+    public function testMultiply5BY5()
     {
         $this->assertSame(25, $this->multiply->multiplyBy5(5));
     }
 
-    public function testB()
+    public function testMultiply5BY4()
     {
         $this->assertEquals(20, $this->multiply->multiplyBy5(4));
     }
 
-    public function testC()
+    public function testMultiply10BY2()
     {
         $this->assertEquals(20, $this->multiply->multiplyBy10(2));
     }
 
-    public function testD()
+    public function testMultiply10BY15()
     {
         $this->assertEquals(150, $this->multiply->multiplyBy10(15));
     }
